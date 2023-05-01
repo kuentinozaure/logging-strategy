f705f4239988bddaa0fadcf55bf1ab3202d4fea2
Quentin AUBRY <aubryquentin700@gmail.com>
2023-05-01 11:05:53 -0400

    add: logLevel logger

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { objectToLog } from './const/objectToLog';
+import { getLogger } from 'loglevel';
 
 @Injectable()
 export class AppService {
+  logObject = objectToLog;
+  private readonly logger = getLogger(AppService.name);
+
   getHello(): string {
+    this.logger.warn(this.logObject);
+    this.logger.error(this.logObject);
+
     return 'Hello World!';
   }
 }
